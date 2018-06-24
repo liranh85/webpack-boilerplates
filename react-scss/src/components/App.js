96ab9219ab1cbe4e23b598f9007469d0f2a96dd1
Liran Harary <liranh85@gmail.com>
2018-06-24 21:30:27 +0100

    react-scss: Correct package.json; npm run format

--- react-scss/src/components/App.js
+++ react-scss/src/components/App.js
@@ -8,7 +8,12 @@ class App extends PureComponent {
   render () {
     return (
       <div>
-        <img src="images/webpack.png" width="225" height="225" alt="Webpack logo" />
+        <img
+          src='images/webpack.png'
+          width='225'
+          height='225'
+          alt='Webpack logo'
+        />
         <h1>Webpack 4 + React + SCSS boilerplate</h1>
         <p>Congratulations, this boilerplate is set up and ready to go.</p>
       </div>
